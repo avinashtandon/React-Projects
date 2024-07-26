0e372cc52a64482d072ea3433afdd3983cbc3678
avinashtandon <avinashtandon07@gmail.com>
2024-07-27 04:22:48 +0530

    Update ToDo.jsx

--- To-Do/ToDo/src/ToDo/ToDo.jsx
+++ To-Do/ToDo/src/ToDo/ToDo.jsx
@@ -1,23 +1,36 @@
 import React from "react";
 
 function ToDo() {
-    const [task, setTask] = React.useState(""); 
-    const handelInputChange = (value) => {
-        setTask(value);
-    };
+  const [task, setTask] = React.useState("");
+  const [data, setData] = React.useState([]);
+  const handelInputChange = (value) => {
+    setTask(value);
+  };
+  const handelFormSubmit = (event) => {
+    event.preventDefault();
+    if (!task) return;
+    if (data.includes(task)) {
+      setTask("");
+      return;
+    }
+    setData((prevData) => [...prevData, task]);
+    setTask("");
+  };
   return (
     <section>
       <header>
         <h1>TO-DO APP</h1>
       </header>
       <section className="Form">
-        <form>
+        <form onSubmit={handelFormSubmit}>
           <div>
             <input
               type="text"
               className="Todo-input"
               placeholder="Enter a new task"
-              autoComplete="off" value={task} onChange={(e)=> handelInputChange(e.target.value)}
+              autoComplete="off"
+              value={task}
+              onChange={(e) => handelInputChange(e.target.value)}
             />
           </div>
           <div>
@@ -26,6 +39,17 @@ function ToDo() {
             </button>
           </div>
         </form>
+      </section>
+      <section className="tasks">
+        <ul>
+          {data.map((currData, index) => {
+            return (
+              <li key={index}>
+                <span>{currData}</span>
+              </li>
+            );
+          })}
+        </ul>
       </section>
     </section>
   );
