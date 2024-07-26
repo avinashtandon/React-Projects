68b1b54fbabcab5b6495d4eb4268c8813654f3e0
avinashtandon <avinashtandon07@gmail.com>
2024-07-26 09:53:23 +0530

    todo

--- To-Do/ToDo/src/ToDo/ToDo.jsx
+++ To-Do/ToDo/src/ToDo/ToDo.jsx
@@ -1,24 +1,30 @@
-import React from 'react'
+import React from "react";
 
 function ToDo() {
   return (
-         <section>
-        <header>
-            <h1>TO-DO APP</h1>
-        </header>
-        <section className='Form'>
-            <form>
-                <div>
-                    <input type='text' className='Todo-input' placeholder='Enter a new task' autoComplete='off'/>
-                </div>
-                <div>
-                    <button className='add-btn' type='Submit'>Add Task</button>
-                </div>
-            </form>
-            
-        </section>
-       </section>
+    <section>
+      <header>
+        <h1>TO-DO APP</h1>
+      </header>
+      <section className="Form">
+        <form>
+          <div>
+            <input
+              type="text"
+              className="Todo-input"
+              placeholder="Enter a new task"
+              autoComplete="off"
+            />
+          </div>
+          <div>
+            <button className="add-btn" type="Submit">
+              Add Task
+            </button>
+          </div>
+        </form>
+      </section>
+    </section>
   );
-};
+}
 
-export default ToDo
+export default ToDo;
